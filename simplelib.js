29e60a7827c9ea2292da52fabea0651e2bd295da
Jake Lisowski <54516275+jmlisowski@users.noreply.github.com>
2020-12-15 16:54:19 -0600

    Update simplelib.js

--- simplelib.js
+++ simplelib.js
@@ -26,16 +26,46 @@ clear =_=> {
     createCanvas(canvas.width, canvas.height);
     background(color);
 }
-rect = (x, y, width, height, color = 'black') => {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, width, height);
+rect = (x, y, width, height, color = 'black', stroke = false) => {
+    if(stroke) {
+        ctx.strokeStyle = color;
+        ctx.strokeRect(x, y, width, height);
+    } else if (!stroke) {
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, width, height);
+    }
     document.body.appendChild(canvas);
 }
-circle = (x, y, radius, color = 'black') => {
+triangle = (x1, y1, x2, y2, x3, y3, color = 'black', stroke = false) => {
     ctx.beginPath();
-    ctx.fillStyle = color;
+    if(stroke) {
+        ctx.strokeStyle = color;
+    } else if (!stroke) {
+        ctx.fillStyle = color;
+    }
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+    if(stroke) {
+        ctx.stroke();
+    } else if (!stroke) {
+        ctx.fill();
+    }
+    document.body.appendChild(canvas);
+}
+circle = (x, y, radius, color = 'black', stroke = false) => {
+    ctx.beginPath();
+    if(stroke) {
+        ctx.strokeStyle = color;
+    } else if (!stroke) {
+        ctx.fillStyle = color;
+    }
     ctx.arc(x, y, radius, 0, 2*Math.PI);
-    ctx.fill();
+    if(stroke) {
+        ctx.stroke();
+    } else if (!stroke) {
+        ctx.fill();
+    }
     document.body.appendChild(canvas);
 }
 text = (string, x, y, color = 'black') => {
@@ -60,7 +90,7 @@ line = (x1, y1, x2, y2, width = 1, color = 'black') => {
     ctx.stroke();
 }
 beginPath = (width = 1, color = 'black') => {
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.lineWidth = width;
 }
